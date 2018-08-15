2e94736767f868dac0c5da6c998e3cbb69f19cd4
Cristina Murillo <cmurill1@jhu.edu>
2018-08-15 19:14:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)
   new_array.size > 0 ? true:false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
